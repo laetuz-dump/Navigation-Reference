85b4fdad27929261d9e7520894a28fb4ba08da43
laetuz <laetuzg@gmail.com>
2023-02-28 15:19:25 +0700

    Setup viewbinding and navigation component.

--- app/src/main/java/com/neotica/navigationreference/MainActivity.kt
+++ app/src/main/java/com/neotica/navigationreference/MainActivity.kt
@@ -2,10 +2,13 @@ package com.neotica.navigationreference
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neotica.navigationreference.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
